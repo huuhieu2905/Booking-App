bff6c93a0e8a8353a8ae29d4399cce9ab0192ed6
huuhieu2905 <huuhieuht38@gmail.com>
2024-10-23 22:29:06 +0700

    Upload Login page

--- api/index.js
+++ api/index.js
@@ -3,15 +3,19 @@ const cors = require('cors');
 const { default: mongoose } = require('mongoose');
 const bcrypt = require('bcryptjs');
 const User = require('./models/User.js');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 require('dotenv').config();
 const app = express();
 
 const bcryptSalt = bcrypt.genSaltSync(10);
+const jwtSecret = 'hieu292929';
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use(cors({
-    Credential: true,
+    credentials: true,
     origin: 'http://localhost:5173',
 }));
 
@@ -35,5 +39,40 @@ app.post('/register', async (req, res) => {
     }
     
 })
+
+app.post('/login', async (req, res) => {
+    const {email, password} = req.body;
+    const userDoc = await User.findOne({email});
+    if (userDoc){
+        const passOk = bcrypt.compareSync(password, userDoc.password);
+        if (passOk) {
+            jwt.sign({email: userDoc.email, 
+                id:userDoc._id, 
+            },  jwtSecret, {}, (err, token)=>{
+                if(err) throw err;
+                res.cookie('token', token, ).json(userDoc);
+            });
+        } else {
+            res.status(422).json('pass not ok');
+        }
+    } else{
+        res.json('not found');
+    }
+});
+
+
+app.get('/profile', (req, res) => {
+    const {token} = req.cookies;
+    if (token){
+        jwt.verify(token, jwtSecret, {},async (err, userData) => {
+            if (err) throw err;
+            const {name, email, _id} = await User.findById(userData.id);
+            res.json({name, email, _id});
+        })
+    }else {
+        res.json(null);
+    }
+  
+});
 
 app.listen(4000);
