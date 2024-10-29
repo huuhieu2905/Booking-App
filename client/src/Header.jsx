cf813b87456e9971ba13208181665cf8e7581915
huuhieu2905 <huuhieuht38@gmail.com>
2024-10-29 21:14:36 +0700

    Modified Header and update Link to login page

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -6,12 +6,12 @@ export default function Header(){
     const {user} = useContext(UserContext);
     return(
         <header className="flex justify-between">
-            <a href="" className="flex items-center gap-1">
+            <Link to={'/'} className="flex items-center gap-1">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-6 -rotate-90">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
                 </svg>
                 <span className="font-bold text-xl">AirBnB</span>
-            </a>
+            </Link>
             <div className="flex gap-2 border border-gray-300 rounded-full py-2 px-4 shadow-md shadow-gray-300">
                 <div>Anywhere</div>
                 <div className="border-l border-gray-300"></div>
