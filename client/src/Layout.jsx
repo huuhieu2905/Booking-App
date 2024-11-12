3ef1c6d22d6956534e98ba000d539804900fcc87
hieuphamtrung23042003 <153638203+hieuphamtrung23042003@users.noreply.github.com>
2024-11-12 22:00:15 +0700

    change layout button

--- client/src/Layout.jsx
+++ client/src/Layout.jsx
@@ -1,66 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
 
-export default function Layout() {
-    return (
+export default function Layout(){
+    return(
         <div className="py-4 px-8 flex flex-col min-h-screen">
             <Header />
             <Outlet />
         </div>
     )
-}import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  
-  </StrictMode>,
-)
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  
-  </StrictMode>,
-)
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  
-  </StrictMode>,
-)
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  
-  </StrictMode>,
-)
+}
