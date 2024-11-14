9f1f7c2cded206c7a00af52148cbd13b86934a32
huuhieu2905 <huuhieuht38@gmail.com>
2024-11-14 22:07:26 +0700

    Remove header link

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -25,7 +25,7 @@ export default function Header(){
 
                 </button>
             </div>
-            <Link to={ user?'/account':'/login'} className="flex items-center gap-2 border border-gray-300 rounded-full py-2 px-4">
+            {/* <Link to={ user?'/account':'/login'} className="flex items-center gap-2 border border-gray-300 rounded-full py-2 px-4">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" class="size-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                 </svg>
@@ -39,7 +39,7 @@ export default function Header(){
                         {user.name}
                     </div>
                 )}
-            </Link>
+            </Link> */}
         </header>
     );
 }
