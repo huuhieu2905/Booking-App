14e6661979c02090726e0880545b811578b54a36
DESKTOP-9J6SJG7\GokuHieu <hieutqp2003@gmail.com>
2024-11-14 14:38:30 +0700

    fix upload by link

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -4,40 +4,40 @@ import axios from "axios";
 import { Navigate } from "react-router-dom";
 import { UserContext } from "../UserContext";
 
-export default function LoginPage() {
+export default function LoginPage(){
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [redirect, setRedirect] = useState(false);
-    const { setUser } = useContext(UserContext);
-    async function handleLoginSubmit(ev) {
+    const {setUser} = useContext(UserContext);
+    async function handleLoginSubmit(ev){
         ev.preventDefault();
-        try {
-            const { data } = await axios.post('/login', { email, password });
+        try{
+            const {data} = await axios.post('/login', {email, password});
             setUser(data);
             alert('Login successful');
             setRedirect(true);
-        } catch (e) {
-            alert('Login failed. Please check your account or your password');
+        }catch(e){
+            alert('Login failed');
         }
     }
     if (redirect) {
-        return <Navigate to={'/'} />
+        return <Navigate to = {'/'} />
     }
     return (
         <div className="mt-4 grow flex items-center justify-around">
             <div className="mb-64">
                 <h1 className="text-4xl text-center mb-4">Login</h1>
                 <form className="max-w-md mx-auto" onSubmit={handleLoginSubmit}>
-                    <input type="email"
-                        placeholder="your_email@gmail.com"
-                        value={email}
-                        onChange={ev => setEmail(ev.target.value)} />
+                    <input type="email" 
+                           placeholder="your@gmail.com"
+                           value={email} 
+                           onChange={ev => setEmail(ev.target.value)}/>
                     <input type="password"
-                        placeholder="password"
-                        value={password}
-                        onChange={ev => setPassword(ev.target.value)} />
+                           placeholder="password" 
+                           value={password} 
+                           onChange={ev => setPassword(ev.target.value)}/>
                     <button className="primary">Login</button>
-                    <div className="text-center py-2 text-gray-300">
+                    <div className="text-center py-2 text-gray-500">
                         Don't have an account yet?
                         <Link className="underline text-black" to={'/register'}> Register now</Link>
                     </div>
