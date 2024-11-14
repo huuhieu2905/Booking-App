5a6e2dcdc03a8675277b7d00815cc1c721d57f2c
DESKTOP-9J6SJG7\GokuHieu <hieutqp2003@gmail.com>
2024-11-14 17:05:16 +0700

    fix upload from local

--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -34,12 +34,29 @@ export default function PlacesPage() {
         );
     }
 
-    async function addPhotoByLink(ev){
+    async function addPhotoByLink(ev) {
         ev.preventDefault();
-        const {data: filename} = await axios.post('/upload-by-link', {link: photoLink});
+        const { data: filename } = await axios.post('/upload-by-link', { link: photoLink });
         console.log(photoLink);
         setAddedPhotos(prev => {
             return [...prev, filename];
+        });
+    }
+
+    function uploadPhoto(ev) {
+        const files = ev.target.files;
+        console.log({files});
+        const data = new FormData();
+        for(let i = 0; i < files.length; i++){
+            data.append('photos', files[i]);
+        }
+        axios.post('/upload', data, {
+            headers: {'Content-Type': 'multipart/form-data'}
+        }).then(response => {
+            const {data: filenames} = response;
+            setAddedPhotos(prev => {
+                return [...prev, ...filenames];
+            });
         });
     }
 
@@ -67,46 +84,48 @@ export default function PlacesPage() {
                             <input value={photoLink}
                                 onChange={ev => setPhotoLink(ev.target.value)}
                                 type="text" placeholder="Add using a link .... jpg" />
-                            <button onClick= {addPhotoByLink} className="bg-gray-200 px-4 rounded-2xl">Add&nbsp;photo</button>
+                            <button onClick={addPhotoByLink} className="bg-gray-200 px-4 rounded-2xl">Add&nbsp;photo</button>
                         </div>
+
                         <div className="mt-2 grid gap-2 grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
                             {addedPhoto.length > 0 && addedPhoto.map(link => (
-                                <div>
-                                    <img className="rounded-2xl" src={'http://localhost:4000/uploads/'+link} alt="" />
+                                <div className="h-32 flex">
+                                    <img className="rounded-2xl w-full object-cover " src={'http://localhost:4000/uploads/' + link} alt="" />
                                 </div>
                             ))}
-                            <button className="flex items-center gap-1 justify-center border bg-transparent rounded-2xl p-8 text-2xl text-gray-600">
+                            <label className="h-32 cursor-pointer flex items-center gap-1 justify-center border bg-transparent rounded-2xl p-8 text-2xl text-gray-600">
+                                <input type="file" multiple className="hidden" onChange={uploadPhoto} />
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M12 16.5V9.75m0 0 3 3m-3-3-3 3M6.75 19.5a4.5 4.5 0 0 1-1.41-8.775 5.25 5.25 0 0 1 10.233-2.33 3 3 0 0 1 3.758 3.848A3.752 3.752 0 0 1 18 19.5H6.75Z" />
                                 </svg>
                                 Upload
-                            </button>
+                            </label>
                         </div>
                         {preInput('Description', 'Description of the place')}
-                        <textarea value={description} onChange={ev=> setDescription(ev.target.value)}/>
+                        <textarea value={description} onChange={ev => setDescription(ev.target.value)} />
                         {preInput('Perks', 'Select all the perks')}
                         <div className="grid mt-2 grid-cols-2 md:grid-cols-4 lg:grid-cols-6">
-                            <Perks selected={ perks} onChange={setPerks} />
+                            <Perks selected={perks} onChange={setPerks} />
                         </div>
                         <h2 className="text-2xl mt-4">Extra info</h2>
                         <p className="text-gray-500 text-sm">house rules, etc</p>
-                        <textarea value={extraInfo} onChange={ev=> setExtraInfo(ev.target.value)}/>
+                        <textarea value={extraInfo} onChange={ev => setExtraInfo(ev.target.value)} />
                         <h2 className="text-2xl mt-4">Check in&out times, max guests</h2>
                         <p className="text-gray-500 text-sm">add check in and out time window for cleaning the room bewtween guests</p>
                         <div className="grid gap-2 sm:grid-cols-3">
                             <div>
                                 <h3 className="mt-2 -mb-1">Check in time</h3>
-                                <input type="text" value={checkIn} onChange={ev=> setCheckIn(ev.target.value)} 
-                                placeholder="14" />
+                                <input type="text" value={checkIn} onChange={ev => setCheckIn(ev.target.value)}
+                                    placeholder="14" />
                             </div>
                             <div>
                                 <h3 className="mt-2 -mb-1">Check out time</h3>
-                                <input type="text" value={checkout} onChange={ev=> setCheckOut(ev.target.value)}
-                                 placeholder="11" />
+                                <input type="text" value={checkout} onChange={ev => setCheckOut(ev.target.value)}
+                                    placeholder="11" />
                             </div>
                             <div>
                                 <h3 className="mt-2 -mb-1">Max number of guests</h3>
-                                <input type="number" value={maxGuest} onChange={ev=> setMaxGuest(ev.target.value)}/>
+                                <input type="number" value={maxGuest} onChange={ev => setMaxGuest(ev.target.value)} />
                             </div>
                         </div>
                         <button className="primary my-4">Save</button>
