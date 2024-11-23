8db224247c5c3943ef4aa1fe90db26a19eaf2f10
HieuTT <hieu.tt210354@sis.hust.edu.vn>
2024-11-23 22:45:29 +0700

    feature: api get all places + index page fe

--- client/src/Perks.jsx
+++ client/src/Perks.jsx
@@ -45,7 +45,7 @@ export default function Perks({ selected, onChange }) {
                 <span>Pets</span>
             </label>
             <label className="border p-4 flex rounded-2xl gap-2 items-center cursor-pointer">
-                <input type="checkbox" checked={selected.includes('')} name="entrance" onChange={handleCbClick} />
+                <input type="checkbox" checked={selected.includes('entrance')} name="entrance" onChange={handleCbClick} />
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" class="size-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 13.5 12 21m0 0-7.5-7.5M12 21V3" />
                 </svg>
--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -1,7 +1,29 @@
-export default function IndexPage(){
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+export default function IndexPage() {
+  const [places, setPlaces] = useState([]);
+
+  useEffect(() => {
+    axios.get('/places').then(res => {
+      setPlaces([...res.data, ...res.data, ...res.data, ...res.data, ...res.data, ...res.data]);
+    }, []);
+  });
+
   return (
-    <div>
-      index page here
+    <div className="mt-8 grid gap-x-6 gap-y-8 grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+      {places.length > 0 && places.map(place => (
+        <div>
+          <div className="bg-gray-500 rounded-2xl flex mb-2">
+            <img className="rounded-2xl object-cover aspect-square" src={"http://localhost:4000/uploads/" + place.photos[0]} alt="" />
+          </div>
+          <h2 className="font-bold">{place.address}</h2>
+          <h3 className="text-sm text-gray-500">{place.title}</h3>
+          <div className="mt-1">
+            <span className="font-bold">${place.price}</span> per night
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
