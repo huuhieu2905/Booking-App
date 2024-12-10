ec8d95e9d7cf68ad062b2c8e0947f22b1c6f042c
HieuTran <hieu.tt210354@sis.hust.edu.vn>
2024-12-11 04:02:21 +0700

    CORS update

--- api/index.js
+++ api/index.js
@@ -22,7 +22,10 @@ app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'));
 app.use(cors({
     credentials: true,
-    origin: "http://localhost:5173",
+    origin: [
+        "http://localhost:5173",
+        "https://hieutt210354.web.app"
+    ]
 }));
 //Fix
 mongoose.connect(process.env.MONGO_URL);
